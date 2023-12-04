ae8f6a2c4dcedea7016514955769d95cc378bee7
phamduong1289 <DuongPAQTS2208036@fpt.edu.vn>
2023-12-04 20:18:22 +0700

    a

--- InternetBanking/Model/InternetBankingContext.cs
+++ InternetBanking/Model/InternetBankingContext.cs
@@ -26,19 +26,20 @@ namespace InternetBanking.Model
         public DbSet<Image>? Images { get; set; }
         public DbSet<Loan>? Loans { get; set; }
         public DbSet<LoanType>? LoanTypes { get; set; }
-    }
-``    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        base.OnModelCreating(builder);
-        //cắt bỏ chữ AspNet trong tên của table
-        var entities = builder.Model.GetEntityTypes();
-        foreach (var entityType in entities)
+        protected override void OnModelCreating(ModelBuilder builder)
         {
-            string name = entityType.Name;
-            if (name!.StartsWith("AspNet"))
+            base.OnModelCreating(builder);
+            //cắt bỏ chữ AspNet trong tên của table
+            var entities = builder.Model.GetEntityTypes();
+            foreach (var entityType in entities)
             {
-                entityType.SetTableName(name.Substring(6));
+                string name = entityType.Name;
+                if (name!.StartsWith("AspNet"))
+                {
+                    entityType.SetTableName(name.Substring(6));
+                }
             }
         }
     }
+    
 }
